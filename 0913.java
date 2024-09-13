9fcaf489d3d0aececa491203f83ca98b7aaf385c
K-jabchae <167736533+KHyeonWoo@users.noreply.github.com>
2024-09-13 13:21:19 +0900

    Array.sort

--- 0913.java
+++ 0913.java
@@ -20,3 +20,41 @@ class Solution {
 /*
 1. HashMap .values() 를 쓰면 Collection 형태 -> forEach 문으로 접근하기!!
 */
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+        //Arrays.sort 는 Integer 형 내림차순 불가능
+        // ArrayList<Integer> sortedCit = new ArrayList<>();
+        // for(int num: citations){
+        //     sortedCit.add(num);
+        // }
+        // Collections.sort(sortedCit,Collections.reverseOrder());
+        
+//         for(int i=0; i<citations.length; i++){
+//             if(i+1 > citations[i]){
+//                 answer = i;
+//                 break;
+//             }
+            
+//             // 끝까지 순회했을 때, i + 1이 sortedCit.get(i)보다 크지 않은 경우
+//             answer = i + 1; 
+//         }
+         for(int i=0; i<citations.length; i++){
+            if(citations.length-i <= citations[i]){
+                answer = citations.length-i;
+                break;
+            }
+            
+        }
+        
+        return answer;
+    }
+}
+/*
+1. Arrays.sort() -> Array , Collections.sort() -> List
+2. Arrays.sort() 는 Integer 내림차순 불가능, 나머지는 Collections 내림차순이랑 동일한 방법
+*/
