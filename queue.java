f805a208375c2b8958fd06bdc5938f370ac9f15a
K-jabchae <167736533+KHyeonWoo@users.noreply.github.com>
2024-09-16 23:34:19 +0900

    Update queue.java

--- queue.java
+++ queue.java
@@ -31,3 +31,38 @@ class Solution {
         return answer;
     }
 }
+
+
+
+
+//트럭 다리 건너기 문제
+import java.util.*;
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        int bridge_weight = 0;
+        int idx = 0;
+        Queue<Integer> que = new LinkedList<>();
+
+        //다리 길이만큼 0 넣고
+        for(int i=0; i<bridge_length; i++){
+            que.offer(0);
+        }
+        //que를 다리라 생각하고 풀면 됨~~
+        while(!que.isEmpty()){
+            answer++;
+            bridge_weight -= que.poll();
+            if(idx < truck_weights.length){
+                if(bridge_weight + truck_weights[idx] <= weight){
+                    bridge_weight += truck_weights[idx];
+                    que.offer(truck_weights[idx]);
+                    idx++;
+                } else{
+                    que.offer(0);
+                }
+            }
+        }
+
+        return answer;
+    }
+}
