a08a6dee316f966cc4f18d65bc6c851e8421ac18
K-jabchae <167736533+KHyeonWoo@users.noreply.github.com>
2024-09-24 12:33:48 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 1.44 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -6,33 +6,42 @@ class Solution {
         str2 = str2.toLowerCase();
         Map<String, Integer> str1Map = new HashMap<>();
         
-        float str1Cnt = 0f;
+        int str1Cnt = 0;
         for(int i=0; i<str1.length()-1; i++){
             String str = str1.substring(i, i+2);
-            if(str.charAt(0) >= 'a' && str.charAt(0) <= 'z' && str.charAt(1) >= 'a' && str.charAt(1) <= 'z'){
+            if(isAlphabet(str)){
                 str1Map.put(str, str1Map.getOrDefault(str, 0) + 1);
                 str1Cnt++;
             }
         }
-        float str2Cnt = 0f;
-        float geouCnt = 0f;
+        int str2Cnt = 0;
+        int geouCnt = 0;
         
         for(int i=0; i<str2.length()-1; i++){
             String str = str2.substring(i, i+2);
-            if(str.charAt(0) >= 'a' && str.charAt(0) <= 'z' && str.charAt(1) >= 'a' && str.charAt(1) <= 'z'){
+            if(isAlphabet(str)){
                 str2Cnt++;
-                if(str1Map.containsKey(str) && str1Map.get(str) > 0){
-                    str1Map.put(str, str1Map.get(str) - 1);
+                int count = str1Map.getOrDefault(str, 0);
+                if(count > 0){
+                    str1Map.put(str, count - 1);
                     geouCnt++;
                 }
             }
         }
-        float hapCnt = str1Cnt + str2Cnt - geouCnt;
+        int hapCnt = str1Cnt + str2Cnt - geouCnt;
         
         if(hapCnt == 0.0){
             return 65536;
         }
         double jaccardSimilarity = (double) geouCnt / hapCnt;
         return (int) (jaccardSimilarity * 65536);
+    }
+    
+    private boolean isAlphabet(String str){
+        if(str.charAt(0) >= 'a' && str.charAt(0) <= 'z' && str.charAt(1) >= 'a' && str.charAt(1) <= 'z'){
+            return true;
+        } else{
+            return false;
+        }
     }
 }
