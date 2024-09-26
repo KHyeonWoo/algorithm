8a3e03bb20d8f1f9a335379b33a367e8f3d510bf
K-jabchae <167736533+KHyeonWoo@users.noreply.github.com>
2024-09-26 14:56:40 +0900

    [level 2] Title: 방문 길이, Time: 21.87 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -5,30 +5,32 @@ class Solution {
     public int solution(String dirs) {
         Set<String> moveSet = new HashSet<>();
         int[] position = {0,0};
-        for(String move: dirs.split("")){
-            int[] current = {position[0], position[1]};
-            if(move.equals("U") && current[1] < 5){
-                current[1] += 1;
-            } else if(move.equals("D") && current[1] > -5){
-                current[1] -= 1;
-            } else if(move.equals("L") && current[0] > -5){
-                current[0] -= 1;
-            } else if(move.equals("R") && current[0] < 5){
-                current[0] += 1;
+        for(char move: dirs.toCharArray()){
+            int dx = position[0];
+            int dy = position[1];
+            if(move == 'U'){
+                dx += 1;
+            } else if(move == 'D'){
+                dx -= 1;
+            } else if(move == 'L'){
+                dy -= 1;
+            } else if(move == 'R'){
+                dy += 1;
             }
-            String str = "";
-            if(position[0] != current[0] || position[1] != current[1]){
-                if(position[0] > current[0] || position[1] > current[1]){
-                    str = Integer.toString(position[0]) +Integer.toString(position[1]) + Integer.toString(current[0]) + Integer.toString(current[1]);
-                } else {
-                    str = Integer.toString(current[0]) +Integer.toString(current[1]) + Integer.toString(position[0]) + Integer.toString(position[1]);
-                }
+            if(dy > 5 || dy < -5 || dx > 5 || dx < -5){
+                continue;
+            }
+            
+            String str1 = position[0] + "" + position[1] + "" + dx + "" + dy;
 
-                moveSet.add(str);
-                position[0] = current[0];
-                position[1] = current[1];
-            }
+            String str2 = dx + "" + dy + "" + position[0] + "" + position[1];
+
+            moveSet.add(str1);
+            moveSet.add(str2);
+            position[0] = dx;
+            position[1] = dy;
+
         }
-        return moveSet.size();
+        return moveSet.size()/2;
     }
 }
